28118b7149f4e6116ddadd9087839d7f087c0d84
Isaiah Juma <isaiah.juma@vault.lishailabs.com>
2026-05-15 15:52:36 +0300

    feat: configured env variables for daraja api

--- config/services.php
+++ config/services.php
@@ -35,4 +35,14 @@ return [
         ],
     ],
 
+    'mpesa' => [
+        'env' => env('MPESA_ENV', 'sandbox'),
+        'consumer_key' => env('MPESA_CONSUMER_KEY'),
+        'consumer_secret' => env('MPESA_CONSUMER_SECRET'),
+        'passkey' => env('MPESA_PASSKEY'),
+        'shortcode' => env('MPESA_SHORTCODE'),
+        'initiator_name' => env('MPESA_INITIATOR_NAME'),
+        'initiator_password' => env('MPESA_INITIATOR_PASSWORD'),
+    ],
+
 ];
